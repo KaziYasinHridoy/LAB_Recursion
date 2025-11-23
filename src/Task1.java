db3588ebbc3c0d970e4d0b3660e655f372014dd4
KaziYasinHridoy <176918255+KaziYasinHridoy@users.noreply.github.com>
2025-11-24 01:21:56 +0600

    Initial commit

---
+++ src/Task1.java
@@ -0,0 +1,45 @@
+import java.util.Scanner;
+public class Task1 { /*
+Task 1:
+    (a)Write a method/function called task1A() that uses a while loop to print from 1 to 10.*/
+    static void task1A() {
+        int i = 1;
+        while (i <= 10)
+            System.out.println(i++);
+    }
+    //(b)Convert the previous task into a recursive function called task1B_recursive().
+    static void task1B_recursive(int i){
+        if (i==11) return;
+        System.out.println(i);
+        task1B_recursive(++i);
+    }
+    /*(c)Write a method/function called task1C() that uses a while loop
+     to print from 1 to N, here N is a user input.      */
+    static void task1C(int N){
+        int i=1;
+        while(i<=N)
+            System.out.println(i++);
+    }
+    //(d)Convert the previous task into a recursive function called task1D_recursive().
+    static void task1D_recursive(int N,int i){
+        if (i>N) return;
+        System.out.println(i);
+        task1D_recursive(N,++i);
+    }
+    public static void main(String[] args) {
+        System.out.println("Task 1A:");
+        task1A();
+
+        System.out.println("\nTask 1B Recursive:");
+        task1B_recursive(1);
+
+        System.out.println("\nTask 1C:");
+        task1C(10);
+
+        System.out.println("\nTask 1D Recursive:");
+        Scanner sc= new Scanner(System.in);
+        System.out.print("Enter N for recursive task: ");
+        int N = sc.nextInt();
+        task1D_recursive(N, 1);
+    }
+}
---
+++ src/Task2.java
@@ -0,0 +1,24 @@
+public class Task2 {
+    static void task2A(int [] arr){
+        int i=0;
+        while(i<arr.length)
+            System.out.println(arr[i++]);
+    }
+    static void task2B_recursive(int []arr,int i){
+        if (i>arr.length) return;
+        System.out.println(i++);
+        task2B_recursive(arr,i);
+    }
+    static int task2C(int []arr){
+        int sum=0;
+        for (int i = 0; i <arr.length ; i++) {
+            sum+=arr[i];
+        }
+        return sum;
+    }
+    static int task2D_recursive(int[] arr, int i){
+        if (i==arr.length) return 0;
+        return arr
+
+    }
+}
